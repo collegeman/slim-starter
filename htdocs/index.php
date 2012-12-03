aaf72285dbbd17bde281bcac3a74e52d84e69e94
Aaron Collegeman <aaron@collegeman.net>
2012-12-03 15:12:32 -0500

    Tweak index.php

--- htdocs/index.php
+++ htdocs/index.php
@@ -5,10 +5,9 @@ define('ROOT', realpath(dirname(__FILE__).'/../'));
 require(ROOT.'/common/bootstrap.php');
 
 // Load any additional libraries:
-require(ROOT.'/common/google/Google_Client.php');
-require(ROOT.'/common/google/contrib/Google_AnalyticsService.php');
-require(ROOT.'/common/google/contrib/Google_Oauth2Service.php');
-require(ROOT.'/common/google.php');
+// require_once(ROOT.'/common/google/Google_Client.php');
+// require_once(ROOT.'/common/google/contrib/Google_Oauth2Service.php');
+// require_once(ROOT.'/common/google.php');
 
 // Load dispatcher, which will run the app:
 require(ROOT.'/dispatcher.php');
