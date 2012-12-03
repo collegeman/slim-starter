c166d793fd26fb7bf7cb276aa1b8a6ca800c3ff0
Aaron Collegeman <aaron@collegeman.net>
2012-12-03 14:49:13 -0500

    Move common project out

--- common/db.php
+++
@@ -1,5 +0,0 @@
-<?php
-require(ROOT.'/common/idiorm.php');
-ORM::configure('mysql:host='.config('db1.host').';dbname='.config('db1.name').';port='.config('db1.port'));
-ORM::configure('username', config('db1.user'));
-ORM::configure('password', config('db1.pass'));
